7b653dc16011856f0540ae9163c5619e1d140931
ETdoFresh <ETdoFresh@gmail.com>
2025-03-09 16:26:08 -0500

    Add inspector scripts for Windows and Unix

---
+++ npx-tools/inspector.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+:: Run the inspector command with any additional arguments
+npx @modelcontextprotocol/inspector node build/index.js %*
---
+++ npx-tools/inspector.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run the inspector command with any additional arguments
+npx @modelcontextprotocol/inspector node build/index.js "$@"
